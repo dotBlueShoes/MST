3ed08512e14a221798111bb528555c81ad1caa3a
dotBlueShoes <matthewstrumillo@gmail.com>
2022-09-17 20:55:55 +0200

    clearing

--- mst/inc/MST/WinAPI/Debug/Console.hpp
+++ mst/inc/MST/WinAPI/Debug/Console.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
-#include <windows.h>
+#include "../Types.hpp"
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <io.h>
 #include <iostream>
 #include <fstream>
-
-#include "../Types.hpp"
 
 namespace mst::winapi::debug::console {
 	
--- mst/inc/mst/Vector.hpp
+++ mst/inc/mst/Vector.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include "BasicTypes.hpp"
 
-
 namespace mst {
 	
 	/// FORWARD DECLARATION
